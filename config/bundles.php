af8ae4ce6890aff2778a085d1ca8d2b72775a9fe
Elmarnissi soufiane <soufiane.elmarnissi@edu.uiz.ac.ma>
2025-01-14 15:27:36 +0000

    Install Foundry and Doctrine fixtures

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
+    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
 ];
