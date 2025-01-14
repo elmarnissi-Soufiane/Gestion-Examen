095f08c8e95d0195ab112384c8e7250dc093e23b
Elmarnissi soufiane <soufiane.elmarnissi@edu.uiz.ac.ma>
2025-01-14 15:49:13 +0000

    Update AppFixtures.php to use the factories. Entities

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,8 +2,16 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\EnseignantFactory;
+use App\Factory\EtudiantFactory;
+use App\Factory\ModuleFactory;
+use App\Factory\NoteFactory;
+use App\Factory\SemestreFactory;
+use App\Factory\UserFactory;
+use App\Factory\FiliereFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use SebastianBergmann\CodeCoverage\Node\File;
 
 class AppFixtures extends Fixture
 {
@@ -11,6 +19,14 @@ class AppFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
+
+        FiliereFactory::createMany(10);
+        SemestreFactory::createMany(10);
+        ModuleFactory::createMany(10);
+        EnseignantFactory::createMany(10);
+        EtudiantFactory::createMany(10);
+        UserFactory::createMany(1);
+        NoteFactory::createMany(10);
 
         $manager->flush();
     }
