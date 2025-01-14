9e4489e1241c1f65db1b98b1c8cb369c17d855eb
Elmarnissi soufiane <soufiane.elmarnissi@edu.uiz.ac.ma>
2025-01-14 15:42:38 +0000

    Add orm-fixtures package for loading sample data in development environment

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
