faba626b5f7704f28b0939c99109dbf40c5e399f
Elmarnissi soufiane <soufiane.elmarnissi@edu.uiz.ac.ma>
2025-01-14 15:36:16 +0000

    Create factories for all entities

---
+++ src/Factory/EnseignantFactory.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Enseignant;
+use App\Repository\EnseignantRepository;
+use Doctrine\ORM\EntityRepository;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+use Zenstruck\Foundry\Persistence\Proxy;
+use Zenstruck\Foundry\Persistence\ProxyRepositoryDecorator;
+
+/**
+ * @extends PersistentProxyObjectFactory<Enseignant>
+ */
+final class EnseignantFactory extends PersistentProxyObjectFactory{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct()
+    {
+    }
+
+    public static function class(): string
+    {
+        return Enseignant::class;
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function defaults(): array|callable
+    {
+        return [
+            'cin' => self::faker()->text(15),
+            'nom' => self::faker()->text(50),
+            'prenom' => self::faker()->text(50),
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    protected function initialize(): static
+    {
+        return $this
+            // ->afterInstantiate(function(Enseignant $enseignant): void {})
+        ;
+    }
+}
---
+++ src/Factory/EtudiantFactory.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Etudiant;
+use App\Repository\EtudiantRepository;
+use Doctrine\ORM\EntityRepository;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+use Zenstruck\Foundry\Persistence\Proxy;
+use Zenstruck\Foundry\Persistence\ProxyRepositoryDecorator;
+
+/**
+ * @extends PersistentProxyObjectFactory<Etudiant>
+ */
+final class EtudiantFactory extends PersistentProxyObjectFactory{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct()
+    {
+    }
+
+    public static function class(): string
+    {
+        return Etudiant::class;
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function defaults(): array|callable
+    {
+        return [
+            'adresse' => self::faker()->text(255),
+            'cne' => self::faker()->text(40),
+            'nom' => self::faker()->text(50),
+            'prenom' => self::faker()->text(50),
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    protected function initialize(): static
+    {
+        return $this
+            // ->afterInstantiate(function(Etudiant $etudiant): void {})
+        ;
+    }
+}
