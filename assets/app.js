d705198695836894ac364e88fa892115db0fe214
Elmarnissi soufiane <soufiane.elmarnissi@edu.uiz.ac.ma>
2025-01-15 13:56:14 +0000

    Install Node.js, Yarn, and dependencies

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,13 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css';
+import './styles/app.scss'; // soufiane
+
+// Soufiane
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
+// Soufiane
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
